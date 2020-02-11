dde97bbf540509df7ba8d4e9554ae64587cf85cd
fractureguy <49120229+fractureguy@users.noreply.github.com>
2020-02-11 16:13:44 -0500

    Populated "activity_login.xml" with a GUI for the login screen. Added skeleton code for responding to button clicks in "LoginActivity.java". Added "MainActivity.java" and "activity_main.xml" files to serve as a placeholder for a child activity of the "LoginActivity.java".

--- app/src/main/java/com/read/restaurantautomationsystem/LoginActivity.java
+++ app/src/main/java/com/read/restaurantautomationsystem/LoginActivity.java
@@ -6,7 +6,11 @@ package com.read.restaurantautomationsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,5 +18,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Bring XML elements to Java.
+        EditText editTextUsername = findViewById(R.id.activity_login_edittext_username);
+        EditText editTextPassword = findViewById(R.id.activity_login_edittext_password);
+        Button button = findViewById(R.id.activity_login_button);
+
+        // Set listener to respond to login button clicks.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /* IMPLEMENT: Call a Firebase function to check if this username and password
+                 *            combination is present in the "users" collection. If it is, send
+                 *            the user to the MainActivity. If not, prompt the user with an error
+                 *            Toast.
+                 * */
+
+                // Assume this combination of username and password is present in the database, send
+                // user to the MainActivity.
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
