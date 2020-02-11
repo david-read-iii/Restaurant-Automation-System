ec18c2ab666ad56d7e5c04862c57186ba846a89a
fractureguy <49120229+fractureguy@users.noreply.github.com>
2020-02-11 09:19:47 -0500

    Configured Firebase to work with this application. Set up area for "LoginActivity.java" and "activity_login.xml" to be filled in. Created "dimens.xml" to keep track of app-wide measurements (i.e. margins, padding, etc.).

---
+++ app/src/main/java/com/read/restaurantautomationsystem/LoginActivity.java
@@ -0,0 +1,18 @@
+//*************************************************************************************************
+// This class... <describe what class does>
+//*************************************************************************************************
+
+package com.read.restaurantautomationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
